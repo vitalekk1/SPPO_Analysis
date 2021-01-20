a0f8a9bab41c514a58921c9c1920e6bd8c61516c
Виталий Каканов <vitalikkakanov55@gmail.com>
2021-01-20 19:55:59 +0500

    First

---
+++ SPPO_Analysis/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SPPO_Analysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RB_Example = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.анализироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LB_Ident = new System.Windows.Forms.ListBox();
+            this.LB_Const = new System.Windows.Forms.ListBox();
+            this.LB_Key = new System.Windows.Forms.ListBox();
+            this.LB_Lim = new System.Windows.Forms.ListBox();
+            this.LB_Conv = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RB_Example
+            // 
+            this.RB_Example.Location = new System.Drawing.Point(12, 31);
+            this.RB_Example.Name = "RB_Example";
+            this.RB_Example.Size = new System.Drawing.Size(294, 407);
+            this.RB_Example.TabIndex = 0;
+            this.RB_Example.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.анализироватьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // анализироватьToolStripMenuItem
+            // 
+            this.анализироватьToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.анализироватьToolStripMenuItem.Name = "анализироватьToolStripMenuItem";
+            this.анализироватьToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.анализироватьToolStripMenuItem.Text = "Анализировать";
+            this.анализироватьToolStripMenuItem.Click += new System.EventHandler(this.анализироватьToolStripMenuItem_Click);
+            // 
+            // LB_Ident
+            // 
+            this.LB_Ident.FormattingEnabled = true;
+            this.LB_Ident.Location = new System.Drawing.Point(327, 31);
+            this.LB_Ident.Name = "LB_Ident";
+            this.LB_Ident.Size = new System.Drawing.Size(224, 121);
+            this.LB_Ident.TabIndex = 2;
+            // 
+            // LB_Const
+            // 
+            this.LB_Const.FormattingEnabled = true;
+            this.LB_Const.Location = new System.Drawing.Point(564, 31);
+            this.LB_Const.Name = "LB_Const";
+            this.LB_Const.Size = new System.Drawing.Size(224, 121);
+            this.LB_Const.TabIndex = 3;
+            // 
+            // LB_Key
+            // 
+            this.LB_Key.FormattingEnabled = true;
+            this.LB_Key.Location = new System.Drawing.Point(327, 167);
+            this.LB_Key.Name = "LB_Key";
+            this.LB_Key.Size = new System.Drawing.Size(224, 121);
+            this.LB_Key.TabIndex = 4;
+            // 
+            // LB_Lim
+            // 
+            this.LB_Lim.FormattingEnabled = true;
+            this.LB_Lim.Location = new System.Drawing.Point(564, 167);
+            this.LB_Lim.Name = "LB_Lim";
+            this.LB_Lim.Size = new System.Drawing.Size(224, 121);
+            this.LB_Lim.TabIndex = 5;
+            // 
+            // LB_Conv
+            // 
+            this.LB_Conv.FormattingEnabled = true;
+            this.LB_Conv.Location = new System.Drawing.Point(449, 308);
+            this.LB_Conv.Name = "LB_Conv";
+            this.LB_Conv.Size = new System.Drawing.Size(224, 121);
+            this.LB_Conv.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LB_Conv);
+            this.Controls.Add(this.LB_Lim);
+            this.Controls.Add(this.LB_Key);
+            this.Controls.Add(this.LB_Const);
+            this.Controls.Add(this.LB_Ident);
+            this.Controls.Add(this.RB_Example);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Лексический Анализатор";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RB_Example;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem анализироватьToolStripMenuItem;
+        private System.Windows.Forms.ListBox LB_Ident;
+        private System.Windows.Forms.ListBox LB_Const;
+        private System.Windows.Forms.ListBox LB_Key;
+        private System.Windows.Forms.ListBox LB_Lim;
+        private System.Windows.Forms.ListBox LB_Conv;
+    }
+}
+
---
+++ SPPO_Analysis/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPPO_Analysis
+{
+    public partial class Form1 : Form
+    {
+
+        static char[] limiters = { ',', '.', '(', ')', '[', ']', ':', ';', '+', '-', '*', '/', '<', '>', '@'};
+        static string[] reservedWords = { "program", "var", "real", "integer", "begin", "for", "downto", "do", "begin", "end", "writeln" };
+        static string allTextProgram;
+        static string temp = "";
+        static int type;
+        static List<string> tableR = new List<string>();    //Таблица ключевых слов
+        static List<string> LConv = new List<string>();     //Таблица 
+        static List<string> tableI = new List<string>();    //Таблица идентификаторов
+        static List<string> tableC = new List<string>();    //Таблица констант
+        static List<string> tableL = new List<string>();    //Таблица разделителей
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearAll()     //Отчистка всех полей
+        {
+            LB_Conv.Items.Clear();
+            LB_Const.Items.Clear();
+            LB_Ident.Items.Clear();
+            LB_Key.Items.Clear();
+            LB_Lim.Items.Clear();
+            tableR.Clear();
+            LConv.Clear();
+            tableI.Clear();
+            tableC.Clear();
+            tableL.Clear();
+            temp = "";
+            type = 0;
+        }
+
+        private void анализироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+
+            allTextProgram = RB_Example.Text;       //Занесение текст из окна ввода в переменную
+
+            Analysis(allTextProgram);               //Запуск Анализатора
+
+            LB_Ident.Items.Add("Идентификаторы:");
+            LB_Ident.Items.AddRange(tableI.ToArray());      //Занесение списка идентифакаторов в поле на форме
+            LB_Const.Items.Add("Константы:");
+            LB_Const.Items.AddRange(tableC.ToArray());
+            LB_Key.Items.Add("Ключевые слова:");
+            LB_Key.Items.AddRange(tableR.ToArray());
+            LB_Lim.Items.Add("Разделители:");
+            LB_Lim.Items.AddRange(tableL.ToArray());
+            LB_Conv.Items.AddRange(LConv.ToArray());
+
+        }
+
+        static void Analysis(string allTextProgram)
+        {
+            for (int i = 0; i < allTextProgram.Length; i++)
+            {
+                
+                if (allTextProgram[i] == '\'')                  //***********
+                {                                               //Поиск апострофов в тексте. Для нахождение строковых констант
+                    temp += '\'';
+                    i++;
+                    while (allTextProgram[i] != '\'')
+                    {
+                        temp += allTextProgram[i];
+                        i++;
+                    }
+                    temp += '\'';
+                    type = 2;
+                    Result(temp);
+                    temp = null;
+                }                                              //*************
+                if (allTextProgram[i] == '{')                  //Нахождение в коде комментариев, чтобы в дальнейшем их не учитывать
+                {
+                    int chet = 1;
+                    while (chet != 0)
+                    {
+                        i++;
+                        if (allTextProgram[i] == '{')
+                            chet++;
+                        if (allTextProgram[i] == '}')
+                            chet--;
+                    }
+                }                                               //**********************
+
+                char nextChar = allTextProgram[i];              //Присваиваем переменной следующий символ строки
+
+                int acsiiCode = (int)nextChar; // Получаем код символа
+                                               // Проверяем относится ли код символа к буквам английского алфавита и знаку нижнего подчеркивания
+                if (((acsiiCode >= 65) && (acsiiCode <= 90)) || ((acsiiCode >= 97) && (acsiiCode <= 122)) || (acsiiCode == 95))
+                {
+                    if (temp == null)
+                        type = 1;
+                    temp += nextChar;
+                    continue;
+                }
+                // Проверяем относится ли код символа к цифрам или к точке
+                if (((acsiiCode >= 48) && (acsiiCode <= 57)) || (acsiiCode == 46))
+                {
+                    // Отдельная проверка, если код символа относиться к точке проверяем, чтобы в переменной temp было число. Если в temp число, значит теперь
+                    // мы считаем его дробным, в противном случае это что-то другое
+                    if (acsiiCode == 46)
+                    {
+                        int out_r;
+                        if (!int.TryParse(temp, out out_r))
+                            goto not_the_number;
+                    }
+                    if (temp == null)
+                        type = 2;
+                    temp += nextChar;
+                    continue;
+                }
+            not_the_number:                                                         //Если символ не число
+                if ((nextChar == ' ' || nextChar == '\n') && temp != null)
+                {
+                    Result(temp);
+                    temp = null;
+                    continue;
+                }
+                // последняя проверка на принадлежность к массиву разделителей. Так же учитываем двойные разделители «:=» и двойные условные разделители
+                foreach (char c in limiters)
+                {
+                    if (nextChar == c)
+                    {
+                        if (temp != null)
+                            Result(temp);
+                        type = 3;
+                        if (nextChar == ':' && allTextProgram[i + 1] == '=')
+                        {
+                            temp = nextChar.ToString() + allTextProgram[i + 1];
+                            Result(temp);
+                            temp = null;
+                            continue;
+                        }
+                        if (nextChar == '<' && (allTextProgram[i + 1] == '>' || allTextProgram[i + 1] == '='))
+                        {
+                            temp = nextChar.ToString() + allTextProgram[i + 1];
+                            Result(temp);
+                            temp = null;
+                            continue;
+                        }
+                        if (nextChar == '>' && allTextProgram[i + 1] == '=')
+                        {
+                            temp = nextChar.ToString() + allTextProgram[i + 1];
+                            Result(temp);
+                            temp = null;
+                            continue;
+                        }
+                        temp = nextChar.ToString();
+                        Result(temp);
+                        temp = null;
+                        continue;
+                    }
+                }
+            }
+
+        }
+
+
+        //Метод Result, где мы определяем, что же мы нашли.
+        //В самом начале проверяем переменную temp на принадлежность к ключевым словам.
+        //Проверяем в начале, чтобы не перепутать найденную лексему с идентификатором.
+        //Если найденное не является ключевым словом, то через switch проверяем тип таблицы, определенный заранее в методе Analysis.
+        static void Result(string temp)
+        {
+            for (int j = 0; j < reservedWords.Length; j++)
+            {
+                if (temp == reservedWords[j])
+                {
+                    for (int i = 0; i < tableR.Count; i++)
+                    {
+                        if (temp == tableR[i])
+                        {
+                            LConv.Add("3    " + i);
+                            return;
+                        }
+                    }
+                    tableR.Add(temp);
+                    LConv.Add("3    " + (tableR.Count - 1));
+                    return;
+                }
+            }
+            switch (type)
+            {
+                case 1:
+                    for (int j = 0; j < tableI.Count; j++)
+                    {
+                        if (temp == tableI[j])
+                        {
+                            LConv.Add("1    " + j);
+                            return;
+                        }
+                    }
+                    tableI.Add(temp);
+                    LConv.Add("1    " + (tableI.Count - 1));
+                    break;
+                case 2:
+                    for (int j = 0; j < tableC.Count; j++)
+                    {
+                        if (temp == tableC[j])
+                        {
+                            LConv.Add("2    " + j);
+                            return;
+                        }
+                    }
+                    tableC.Add(temp);
+                    LConv.Add("2    " + (tableC.Count - 1));
+                    break;
+                case 3:
+                    for (int j = 0; j < tableL.Count; j++)
+                    {
+                        if (temp == tableL[j])
+                        {
+                            LConv.Add("4    " + j);
+                            return;
+                        }
+                    }
+                    tableL.Add(temp);
+                    LConv.Add("4    " + (tableL.Count - 1));
+                    break;
+            }
+        }
+
+
+    }
+}
